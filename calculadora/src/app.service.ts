9b864824beab749c6c9e82ac75790786414853dd
Dsmcamila <domenica.sanchez@epn.edu.ec>
2025-05-16 09:00:02 -0500

    Class 02: Continue Calculator

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -3,6 +3,41 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    console.log('log!');
+    console.debug('debug!');
+    console.info('info!');
+    console.warn('warn!');
+    console.error('error!');
+    return 'Hello World!!';
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
